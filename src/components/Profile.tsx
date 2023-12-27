6ce084bb933fac14c61988accf2de25f86838f45
ageu_dev19 <ageumenezes423@gmail.com>
2023-12-27 15:48:04 -0300

    Add modal to change the user name

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,20 +1,75 @@
-import { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import styles from '../styles/pages/Profile.module.scss';
 import { ChallengesContext } from '@/contexts/ChallengesContext';
 
-export function Profile() {
-  const {level} = useContext(ChallengesContext);
+interface handleTextareaKeyDownProps {
+  key: string;
+  shiftKey: any;
+  preventDefault: () => void;
+}
 
-  return(
-    <div className={styles.profileContainer}>
-      <img src="https://avatars.githubusercontent.com/u/89534178?v=4" alt="Ageu Menezes" />
-      <div>
-        <strong>Ageu Menezes</strong>
-        <p>
-          <img src="icons/level.svg" alt="" />
-          Level {level}
-        </p>
-      </div>
+export function Profile() {
+  const { level } = useContext(ChallengesContext);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [editableName, setEditableName] = useState("Adicionar Nome");
+
+  const handleEditClick = () => {
+    setIsModalOpen(true);
+  };
+
+  const handleSaveClick = () => {
+    if (validateName(editableName)) {
+      setIsModalOpen(false);
+    } else {
+      alert('Por favor, insira um nome válido.');
+    }
+  };
+
+  const handleCancelClick = () => {
+    setIsModalOpen(false);
+  };
+
+  const handleTextareaKeyDown = (e: handleTextareaKeyDownProps) => {
+    if (e.key === 'Enter' && !e.shiftKey) {
+      e.preventDefault();
+      handleSaveClick();
+    }
+  };
+
+  const validateName = (name: string) => {
+    return name.trim() !== '' && name.split('\n').length === 1 && name.length <= 28;
+  };
+
+  return (
+    <div className={`${isModalOpen ? styles.modalOpen : styles.profileContainer}`}>
+      {!isModalOpen && (
+        <>
+          <img src="/FitFlow.PNG" alt="Nome do Usuário" />
+          <div>
+            <strong>
+              <span onClick={handleEditClick} style={{ cursor: 'pointer' }}>
+                {editableName}
+              </span>
+            </strong>
+            <p>
+              <img src="icons/level.svg" alt="" />
+              Level {level}
+            </p>
+          </div>
+        </>
+      )}
+
+      {isModalOpen && (
+        <div className={styles.modal}>
+          <textarea
+            value={editableName}
+            onChange={(e) => setEditableName(e.target.value)}
+            onKeyDown={handleTextareaKeyDown}
+          />
+          <button className={styles.saveB} onClick={handleSaveClick}>Salvar</button>
+          <button className={styles.cancelB} onClick={handleCancelClick}>Cancelar</button>
+        </div>
+      )}
     </div>
   );
 }
